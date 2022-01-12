7d6c7947b63c08beefcd509cdfadce8989a5c083
gemsvido <kid4cars@gmail.com>
2022-01-12 09:02:12 -0800

    Retard Development Kit

--- src/rdk/Testing/Tutorial.java
+++ src/rdk/Testing/Tutorial.java
@@ -3,13 +3,17 @@ package rdk.Testing;
 import rdk.Annotations.RDK;
 import rdk.Annotations.Retarded;
 import rdk.Classes.Retard;
+import rdk.Interfaces.RetardFace;
 
 public class Tutorial {
 
     public static void main(String[] args) {
 
         @Retarded @RDK String rtrd = "Welcome to RDK.";
+
         Retard.out.println(rtrd);
+
+        RetardFace.RetardedKid.strip();
 
     }
 }
